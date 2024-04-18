7699560b7bc68503f26fe05f5977f1a7bbd7f3f9
AbdullohDev07 <gamesupro01@gmail.com>
2024-04-18 18:52:38 +0500

    pushTwo

---
+++ src/app/modules/admin/pricing/modern/modern.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PricingModernComponent } from 'app/modules/admin/pricing/modern/modern.component';
+
+export default [
+    {
+        path     : '',
+        component: PricingModernComponent,
+    },
+] as Routes;
---
+++ src/app/modules/admin/pricing/single/single.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PricingSingleComponent } from 'app/modules/admin/pricing/single/single.component';
+
+export default [
+    {
+        path     : '',
+        component: PricingSingleComponent,
+    },
+] as Routes;
